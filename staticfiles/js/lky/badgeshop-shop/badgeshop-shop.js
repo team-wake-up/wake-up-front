78958eb95c826da54c8fc7415c371797f5dd1f77
qnsekddl1 <qnsekddl1@naver.com>
2024-02-08 15:03:57 +0900

    이기영 badgeshop/shop Done

--- staticfiles/js/lky/badgeshop-shop/badgeshop-shop.js
+++ staticfiles/js/lky/badgeshop-shop/badgeshop-shop.js
@@ -1,17 +1,17 @@
-// const sortP = document.querySelector('.check-input');
-// sortP.addEventListener('click', (e) => {
-//     // alert('성공');
-//     sortP.style.color = 'black';
+// // const sortP = document.querySelector('.check-input');
+// // sortP.addEventListener('click', (e) => {
+// //     // alert('성공');
+// //     sortP.style.color = 'black';
+// // });
+
+// const selected = document.querySelector('select[name=sortOption]');
+// const options = document.querySelectorAll('select[name=sortOption]');
+
+// selected.addEventListener('change', (e) => {
+//     // console.log(e.target.style.display);
+//     if (e.target.value === 'sortNewestDate') {
+//         e.target.style.display = 'none';
+//     } else {
+//         alert(e.target.value);
+//     }
 // });
-
-const selected = document.querySelector('select[name=sortOption]');
-const options = document.querySelectorAll('select[name=sortOption]');
-
-selected.addEventListener('change', (e) => {
-    console.log(e.target.style.display);
-    if (e.target.value === 'sortNewestDate') {
-        e.target.style.display = 'none';
-    } else {
-        alert(e.target.value);
-    }
-});
